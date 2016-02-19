2c1e8011798d87d3089df9a814c24588cdcd7c06
root <root@test.rikoten.com>
2016-02-19 10:17:44 +0000

    mysqlの設定を見直した。（パスワード有効期限切れに対する対処等）

--- chef/cookbooks_site/rikoten/recipes/default.rb
+++ chef/cookbooks_site/rikoten/recipes/default.rb
@@ -216,19 +216,18 @@ end
 #
 # mysql導入・設定
 #
+yum_package "mysql-community-server" do
+	action :install
+	options "--enablerepo=mysql57-community"
+end
+service "mysqld" do
+	action [:enable]
+end
 
-passForMySQL="rikotenMySQL"
-
-result = shell_out('MYSQL_PWD="'+passForMySQL+'" mysql -u root -N -e "" --connect-expired-password')
-
+# rootパスワード設定
+passForMySQL="Rikoten!5621Y"
+result = shell_out('MYSQL_PWD="'+passForMySQL+'" mysql -u root -N -e ""')
 unless result.exitstatus == 0 then
-	yum_package "mysql-community-server" do
-		action :install
-		options "--enablerepo=mysql57-community"
-	end
-	service "mysqld" do
-		action [:enable]
-	end
 	template "/etc/my.cnf" do
 		source "my.cnf.erb"
 		mode "644"
@@ -249,20 +248,29 @@ unless result.exitstatus == 0 then
 		returns [0]
 		retries 3
 	end
-	# 設定ファイルを通常モードに書き換え
-	template "/etc/my.cnf" do
-		source "my.cnf.erb"
-		mode "644"
-		owner "root"
-		group "root"
-		notifies :restart, "service[mysqld]", :immediately
-		action :create
-		variables({
-			:c_opt => "",
-		})
-	end
 end
 
+# 設定ファイルを通常モードに書き換え
+template "/etc/my.cnf" do
+	source "my.cnf.erb"
+	mode "644"
+	owner "root"
+	group "root"
+	notifies :restart, "service[mysqld]", :immediately
+	action :create
+	variables({
+		:c_opt => "",
+	})
+end
+# 期限切れになっている場合はそれを解消する
+bash 'apply_mysql_root_pass' do
+	user 'mysql'
+	code <<-EOC
+		MYSQL_PWD="#{passForMySQL}" mysql -u root -N -e "SET PASSWORD='#{passForMySQL}';" --connect-expired-password
+	EOC
+	returns [0]
+	retries 3
+end
 #
 # Digest認証ファイルを配置
 #
